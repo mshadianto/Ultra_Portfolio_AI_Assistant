07bd7a042441bf37fff8bf32c3612274f8254e6b
MS Hadianto <148893248+mshadianto@users.noreply.github.com>
2025-06-03 01:25:06 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ from PIL import Image
 import base64
 
 st.set_page_config(
-    page_title="Ultra Portfolio AI Assistant",
+    page_title="Investment Portfolio AI Assistant",
     page_icon="💼",
     layout="wide"
 )
